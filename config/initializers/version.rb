2a186f721a251f8852dadc535aa9e2a22f2b4c5e
EvilJadeon <thejadeon@vk.com>
2021-10-28 19:01:03 +0300

    Исправил ошибки (удалил все require)

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,11 +1,3 @@
-require_relative "boot"
-
-require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
 module Testguru
   class Application < Rails::Application
     VERSION = '0.1.0'.freeze
