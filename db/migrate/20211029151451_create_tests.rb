72f72af17580217001c032b477e713ca3a40624c
EvilJadeon <thejadeon@vk.com>
2021-10-29 19:42:10 +0300

    Add column 'author_id'

--- db/migrate/20211029151451_create_tests.rb
+++ db/migrate/20211029151451_create_tests.rb
@@ -3,10 +3,9 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, null: false, default: 0
-      t.string :author, null: false
 
       t.references :category, foreign_key: { to_table: 'categories' }
-      t.references :user, foreign_key: { to_table: 'users' }
+      t.references :author, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end
