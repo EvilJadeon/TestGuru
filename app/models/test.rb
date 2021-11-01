b20ef01626bc5dbf22a0d44044b20d3e567c23be
EvilJadeon <thejadeon@vk.com>
2021-11-01 23:51:49 +0300

    Create class method 'test_names' in Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.test_names(category)
+    joins("INNER JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category).pluck(:title)
+  end
 end
