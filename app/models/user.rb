ea82856d7cf4c201bc68ced041a307a385ec3321
EvilJadeon <thejadeon@vk.com>
2021-11-03 17:46:03 +0300

    Add assotiations 'has_many' and 'has_many_through'

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
     Test.joins('INNER JOIN results ON results.test_id = tests.id')
       .where(tests: {level: level}, results: {user_id: id})
   end
+
+  has_many :tests
+
+  has_many :results
+  has_many :tests, through: :results
 end
