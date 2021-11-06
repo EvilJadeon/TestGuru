40d160a885a32c0487a384e968e102a907b435b8
EvilJadeon <thejadeon@vk.com>
2021-11-06 22:54:24 +0300

    Add scopes and validations into models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
 
+  validates :name, presence: true
+  validates :email, presence: true
+
   def passed_tests(test_level)
-    Test.joins(:results)
-      .where(tests: {level: test_level}, results: {user: id})
+    Test.joins(:result)
+    .where(tests: {level: test_level}, results: {user: id})
   end
 end
