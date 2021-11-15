72ee21831bfe810745e3a6e7fd64af0a4f85c99a
EvilJadeon <thejadeon@vk.com>
2021-11-15 23:58:09 +0300

    Fix unbelievable mistakes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,14 @@
 class Answer < ApplicationRecord
-    belongs_to :question
+  belongs_to :question
 
-    validates :body, presence: true
-    validates :number_of_answers, on: :create
-  
-    scope :correct_answer, -> { where(correct: true) }
-  
-    private
-  
-    def number_of_answers
-      errors.add(:base, 'Count of answers must be 1 to 4') if question.answers.count >= 4
-    end
+  validates :body, presence: true
+  validates :number_of_answers, on: :create
+
+  scope :correct_answer, -> { where(correct: true) }
+
+  private
+
+  def number_of_answers
+    errors.add(:base, 'Count of answers must be 1 to 4') if question.answers.count >= 4
+  end
 end
