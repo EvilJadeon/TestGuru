d37aa11f1538aca2c3140026eed16eef0399bfb0
EvilJadeon <thejadeon@vk.com>
2021-11-17 10:46:46 +0300

    !

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   has_many :having_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
@@ -9,4 +10,5 @@ class User < ApplicationRecord
   def passed_tests(test_level)
     tests.where(level: test_level)
   end
+  
 end
