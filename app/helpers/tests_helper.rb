e2b978194d045cd70ed5be2558c6b4a283f0f29a
EvilJadeon <thejadeon@vk.com>
2021-11-18 18:43:58 +0300

    Add views for 'tests_controller', 'questions_controller'

---
+++ app/helpers/tests_helper.rb
@@ -0,0 +1,8 @@
+module TestsHelper
+  TEST_LEVELS = { 0 => :beginner, 1 => :elementary, 
+                  2 => :intermediate, 3 => :advanced }.freeze
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hero
+  end
+end
