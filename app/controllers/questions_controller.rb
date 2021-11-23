c9818445942cb6456f7bb5684c591fbb6af16d7a
EvilJadeon <thejadeon@vk.com>
2021-11-23 17:21:56 +0300

    Created 'TestPassage' model, controller, views and made application logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -18,7 +18,7 @@ class QuestionsController < ApplicationController
     @question = @test.questions.new(question_params)
 
     if @question.save
-      redirect_to test_path(@test)
+      redirect_to test_path(@question.test)
     else
       render :new
     end
@@ -26,7 +26,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to test_path(@test)
+      redirect_to test_path(@question.test)
     else
       render :edit
     end
@@ -34,7 +34,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    redirect_to test_path
+    redirect_to test_path(@question.test)
   end
 
   private
