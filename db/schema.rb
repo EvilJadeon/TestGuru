ae2455d5116cbb51e47bfb095db94267681e9ebf
EvilJadeon <thejadeon@vk.com>
2021-11-23 18:21:30 +0300

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,14 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_11_22_161525) do
 
@@ -51,7 +62,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_161525) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email", null: false
   end
 
   add_foreign_key "answers", "questions"
