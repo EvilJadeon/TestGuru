b214036958263050893917c6f3d2f664dbbe407d
EvilJadeon <thejadeon@vk.com>
2021-11-25 19:24:28 +0300

    Add email validation, password_secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
 
   has_many :having_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
@@ -5,8 +7,9 @@ class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  validates :name, presence: true
-  validates :email, presence: true
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+
+  has_secure_password
 
   def passed_tests(test_level)
     tests.where(level: test_level)
@@ -15,5 +18,4 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
-  
 end
