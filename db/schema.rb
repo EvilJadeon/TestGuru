2b43ca428753fe3ffbf6198a97d5f6f72163cf79
EvilJadeon <thejadeon@vk.com>
2021-11-25 19:25:32 +0300

    Create session new view

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_11_22_161525) do
+ActiveRecord::Schema.define(version: 2021_11_24_104423) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -64,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_161525) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
