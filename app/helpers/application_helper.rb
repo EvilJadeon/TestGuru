670eb4cd7d13e87c112be92e54f3b4248f838acd
EvilJadeon <thejadeon@vk.com>
2021-11-25 19:23:30 +0300

    Auto-create helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def current_year
     Date.current.year
   end
@@ -6,4 +7,5 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'Click me', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
 end
