f51e8d42e75179db268ee8aab4fc9235d886e17b
EvilJadeon <thejadeon@vk.com>
2021-12-02 13:35:23 +0300

    Rename resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for(user_or_admin)
-    user_or_admin.is_a?(Admin) ? admin_tests_path : root_path
+  def after_sign_in_path_for(user)
+    user.is_a?(Admin) ? admin_tests_path : root_path
   end
 
   def configure_permitted_parameters
