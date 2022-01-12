58aac4167df0486c63047cc3eaebb1fe53a15f73
EvilJadeon <thejadeon@vk.com>
2022-01-12 12:05:01 +0300

    Add callbacks

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
   class Admin::TestsController < Admin::BaseController
-    before_action :find_test, only: %i[show edit update destroy]
 
-    def index
-      @tests = Test.all
-    end
+    before_action :find_tests, only: %i[index update_inline]
+    before_action :find_test, only: %i[show edit update destroy update_inline]
+
+    def index; end
 
     def show
       @test
@@ -35,12 +35,24 @@
       end
     end
 
+    def update_inline
+      if @test.update(test_params)
+        redirect_to admin_tests_path
+      else
+        render :index
+      end
+    end
+
     def destroy
       @test.destroy
       redirect_to admin_tests_path, notice: t('.delete')
     end
 
     private
+
+    def find_tests
+      @tests = Test.all
+    end
 
     def find_test
       @test = Test.find(params[:id])
