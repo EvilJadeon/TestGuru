d13b144216d4cd90627d6c306faf39a3393f86de
EvilJadeon <thejadeon@vk.com>
2022-01-17 18:46:43 +0300

    Little changes

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -4,8 +4,8 @@ document.addEventListener('turbolinks:load', function() {
   let errors = document.querySelector('.resource-errors')
 
   if (errors) {
-      let resourceId = errors.dataset.resourceId
-      formInlineHandler(resourceId)
+    let resourceId = errors.dataset.resourceId
+    formInlineHandler(resourceId)
   }
 })
 
@@ -19,17 +19,13 @@ function formInlineHandler(testId) {
   let link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
   let $testTitle = $('.test-title[data-test-id="' + testId + '"]')
   let $formInline = $('.form-inline[data-test-id="' + testId + '"]')
-
-  if (!($formInline && $testTitle && link)) {
-      return
-  }
-
-  $testTitle.toggle()
+ 
   $formInline.toggle()
+  $testTitle.toggle()
 
   if ($formInline.is(':visible')) {
-      link.textContent = link.dataset.textCancel
+      link.textContent = 'Cancel'
   } else {
-      link.textContent = link.dataset.textEdit
+      link.textContent = 'Edit'
   }
 } 
