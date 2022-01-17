0ce5778f251ab6dd91779b6bdb99cd1445c7fcae
EvilJadeon <thejadeon@vk.com>
2022-01-17 18:47:03 +0300

    Create JS for 'progress-bar'

--- app/javascript/utilities/progress_bar.js
+++ app/javascript/utilities/progress_bar.js
@@ -1,18 +1,12 @@
 document.addEventListener('turbolinks:load', function () {
-  let control = document.querySelector('.submit')
+  let progressBar = document.querySelector('.progressbar')
 
-  if (control) {
-      currentProgress()
-      control.addEventListener('click', currentProgress)
+  if (progressBar) {
+    let currentQuestionNumber = progressBar.dataset.currentQuestionNumber
+    let totalQuestions = progressBar.dataset.totalQuestions
+    let progress = Math.round(currentQuestionNumber / totalQuestions * 100)
+
+    progressBar.style.width = progress + '%'
+    progressBar.innerHTML = progress + '%'
   }
 })
-
-function currentProgress(event) {
-  let current_question = document.querySelector('.bar').dataset.currentNumber - 1
-  let total_questions = document.querySelector('.bar').dataset.totalQuestions
-  let result_percentage = (100 / total_questions).toFixed(0) * current_question
-  let item = document.querySelector('.progress-bar');
-
-  item.style.width = result_percentage + "%";
-  item.innerHTML = result_percentage + "%";
-} 
