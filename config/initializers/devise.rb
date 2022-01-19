d77856fca2805fcd19621064162a2c95f9748e30
EvilJadeon <thejadeon@vk.com>
2022-01-19 23:00:23 +0300

    Change 'mailer', 'mailer_sender'

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,10 +24,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'mail@testguru.com'
+  config.mailer_sender = 'thejadeonwot@gmail.com'
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'MailForm::Base'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
