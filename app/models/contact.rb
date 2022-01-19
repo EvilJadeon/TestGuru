fc39888477cc0dbc7c5ba23406c3a2064bc45fc9
EvilJadeon <thejadeon@vk.com>
2022-01-19 20:28:53 +0300

    Create form for feedbacks

---
+++ app/models/contact.rb
@@ -0,0 +1,15 @@
+class Contact < MailForm::Base
+  attribute :name
+  attribute :email
+  attribute :message
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    { 
+      subject: "My contact form",
+      to: "thejadeonwot@gmail.com",
+      from: %("#{name}" <#{email}">)
+    }
+  end
+end
