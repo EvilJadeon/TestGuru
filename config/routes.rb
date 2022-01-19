d098980697c14aff705d5d7a7f76428f76f05f09
EvilJadeon <thejadeon@vk.com>
2022-01-19 15:25:35 +0300

    Add 'except: :idex'

--- config/routes.rb
+++ config/routes.rb
@@ -26,8 +26,8 @@ Rails.application.routes.draw do
     resources :tests do
       patch :update_inline, on: :member
 
-      resources :questions, shallow: true do
-        resources :answers, shallow: true
+      resources :questions, except: :index, shallow: true do
+        resources :answers, except: :index, shallow: true
       end
     end
   end
