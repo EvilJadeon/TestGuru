e35b404e70f2633c70f5f0b9a220918542994b38
EvilJadeon <thejadeon@vk.com>
2022-01-19 17:24:06 +0300

    add domain to smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'thejadeontestguru.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
+    domain: 'gmail.com',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
