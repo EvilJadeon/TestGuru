0c979aec331f607ef412d0c99bd83bfe2aa9666d
EvilJadeon <thejadeon@vk.com>
2022-01-20 12:52:30 +0300

    !

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -10,7 +10,7 @@ class TestsController < ApplicationController
   def start
     @test = Test.find(params[:id])
 
-    if current_user.test_passage.total_questions > 0
+    if current_user.test_passage(@test).total_questions > 0
       current_user.tests.push(@test)
       redirect_to current_user.test_passage(@test)
     else
