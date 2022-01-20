f0ef411b8dc713a040130e72b20b63298fecdd28
EvilJadeon <thejadeon@vk.com>
2022-01-20 21:46:37 +0300

    Add index

--- db/migrate/20211122160429_create_test_passages.rb
+++ db/migrate/20211122160429_create_test_passages.rb
@@ -4,7 +4,7 @@ class CreateTestPassages < ActiveRecord::Migration[6.1]
   def change
     create_table :test_passages do |t|
       t.references :user, null: false, foreign_key: { on_delete: :cascade }
-      t.references :test, null: false, foreign_key: { on_delete: :cascade }
+      t.references :test, null: false, index: true, foreign_key: { on_delete: :cascade }
       t.references :current_question, foreign_key: { to_table: :questions }
       t.integer :correct_questions, default: 0
 
