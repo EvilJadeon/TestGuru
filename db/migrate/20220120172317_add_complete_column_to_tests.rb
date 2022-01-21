a196c619c4aa36e96e9701dad1ba2206151e8d76
EvilJadeon <thejadeon@vk.com>
2022-01-21 21:49:06 +0300

    # frozen string literal true

--- db/migrate/20220120172317_add_complete_column_to_tests.rb
+++ db/migrate/20220120172317_add_complete_column_to_tests.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddCompleteColumnToTests < ActiveRecord::Migration[6.1]
   def change
     add_column :tests, :completed, :boolean, null: false, default: false
