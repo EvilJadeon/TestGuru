1630eccce877942be9ab5d331e9423d36e9a7b6f
EvilJadeon <thejadeon@vk.com>
2022-01-21 12:44:25 +0300

    Change correct flash type

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -14,7 +14,7 @@ class TestsController < ApplicationController
       current_user.tests.push(@test)
       redirect_to current_user.test_passage(@test)
     else
-      redirect_to root_path, alert: 'Нет доступа к тесту!'
+      redirect_to root_path, error: 'Нет доступа к тесту!'
     end
   end
 end
