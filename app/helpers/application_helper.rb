ef67228c9b9f339ce27d1929266537d91485753c
EvilJadeon <thejadeon@vk.com>
2022-01-21 12:49:50 +0300

    Change flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,12 @@
 module ApplicationHelper
   FLASH_TYPES = {
     notice: 'alert-info',
-    error: 'alert-warning',
+    warning: 'alert-warning',
     success: 'alert-success'
-  }.freeze
+  }
 
   def current_year
-    Date.current.year
+    Date.current.years
   end
 
   def github_url(author, repo)
