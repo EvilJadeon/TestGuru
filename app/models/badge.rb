c9c9d312142a131b3abbd57e841b0897ce70de08
EvilJadeon <thejadeon@vk.com>
2022-02-16 23:08:42 +0300

    Change badge rules

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,12 +1,12 @@
 class Badge < ApplicationRecord
   has_one_attached :image
-  has_many :users_badges, dependent: :destroy
+  has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
   RULES = {
-    1 => 'First try pass test',
-    2 => 'Lose 10 tests',
-    3 => 'Pass all tests of a category',
-    4 => 'Pass all tests of a level'
+    'first_try_pass' => 'First try pass test',
+    'lose_10_tests' => 'Lose 10 tests',
+    'pass_tests_category' => 'Pass all tests of a category',
+    'pass_tests_level' => 'Pass all tests of a level'
   }.freeze
 end
