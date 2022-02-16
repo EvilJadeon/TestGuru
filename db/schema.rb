2fca9b44df4b9d6d0398a72ea4f5d20e67e291fb
EvilJadeon <thejadeon@vk.com>
2022-02-16 23:11:38 +0300

    Change migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_104058) do
+ActiveRecord::Schema.define(version: 2022_02_16_192421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,10 +53,11 @@ ActiveRecord::Schema.define(version: 2022_01_28_104058) do
   end
 
   create_table "badges", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "rule", default: 0, null: false
+    t.string "title"
+    t.string "rule"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "value"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -90,6 +91,7 @@ ActiveRecord::Schema.define(version: 2022_01_28_104058) do
     t.integer "correct_questions", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "successfull", default: false
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
@@ -106,6 +108,15 @@ ActiveRecord::Schema.define(version: 2022_01_28_104058) do
     t.integer "timer"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "user_badges", force: :cascade do |t|
+    t.bigint "badge_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["badge_id"], name: "index_user_badges_on_badge_id"
+    t.index ["user_id"], name: "index_user_badges_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -134,15 +145,6 @@ ActiveRecord::Schema.define(version: 2022_01_28_104058) do
     t.index ["type"], name: "index_users_on_type"
   end
 
-  create_table "users_badges", force: :cascade do |t|
-    t.bigint "badge_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["badge_id"], name: "index_users_badges_on_badge_id"
-    t.index ["user_id"], name: "index_users_badges_on_user_id"
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
@@ -154,6 +156,6 @@ ActiveRecord::Schema.define(version: 2022_01_28_104058) do
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
-  add_foreign_key "users_badges", "badges"
-  add_foreign_key "users_badges", "users"
+  add_foreign_key "user_badges", "badges"
+  add_foreign_key "user_badges", "users"
 end
