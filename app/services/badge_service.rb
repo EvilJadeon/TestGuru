a0175421b1dbe3ca2cf3e78e9d4785f6242a0a1a
EvilJadeon <thejadeon@vk.com>
2022-02-17 15:15:22 +0300

    Change rules so good!

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -14,31 +14,36 @@ class BadgeService
   private
 
   def pass_tests_category(value)
-    find_tests = Test.where(category_id: @test.category_id).count
-    find_test_passages = Test.joins(:test_passages)
-                              .where('test_passages.user_id = ? and test_passages.test_id = ? and test_passages.successfull = ? and tests.category_id = ?',
-                              @test_passage.user_id, @test_passage.test_id, true, value.to_i).distinct.count
+    find_tests = Test.test_names_title(value).count
+
+    find_test_passages = Test.where(category: value)
+                              .joins(:test_passages)
+                              .where(test_passages: { user: @user, successfull: true })
+                              .distinct.count
     
     find_tests == find_test_passages
   end
 
   def first_try_pass(value)
-    find_test_passages = TestPassage.where('test_id = ? and user_id = ? and successfull = ?', @test_passage.test_id, @test_passage.user_id, true).distinct.count
-    find_test_passages == 1
-    
+    find_success_test_passages = TestPassage.where(user: @user, test: @test, successfull: true).count
+    find_fall_test_passages = TestPassage.where(user: @user, test: @test, successfull: false).count
+
+    find_success_test_passages == 1 && find_fall_test_passages == 0
   end
 
   def lose_10_tests(value)
-    find_test_passages = TestPassage.where('test_id = ? and user_id = ? and successfull = ?', @test_passage.test_id, @test_passage.user_id, false).count
+    find_test_passages = TestPassage.where(user: @user, test: @test, successfull: false).count
 
-    find_test_passages == 10
+    find_test_passages % 10 == 0
   end
 
   def pass_tests_level(value)
-    find_tests = Test.where(level: @test.level).count
-    find_test_passages = Test.joins(:test_passages)
-                            .where('test_passages.user_id = ? and test_passages.test_id = ? and test_passages.successfull = ? and tests.level = ?',
-                            @test_passage.user_id, @test_passage.test_id, true, value.to_i).distinct.count
+    find_tests = Test.where(level: value.to_i).count
+
+    find_test_passages = Test.where(level: value.to_i)
+                              .joins(:test_passages)
+                              .where(test_passages: { user: @user, successfull: true })
+                              .distinct.count
     
     find_tests == find_test_passages
   end
